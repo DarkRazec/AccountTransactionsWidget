b15095a3c0e8347d75ad6c5bedb92baaf7b546e3
DarkRazec <darkrazec@yahoo.com>
2024-01-21 22:59:24 +0300

    added docstrings to functions

--- src/sort_data.py
+++ src/sort_data.py
@@ -2,4 +2,10 @@ from operator import itemgetter
 
 
 def sort_data(data: list[dict], reversed=False):
+    """
+    Возвращает отсортированный список словарей по статусу и дате
+    :param data: исходный список
+    :param reversed: флаг для вывода списка в обратном порядке
+    :return: отсортированный список
+    """
     return sorted(data, key=itemgetter('state', 'date'), reverse=reversed)
